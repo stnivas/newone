8b3c3ab34af7846c640f5d3b220601559a1da438
stnivas <86654987+stnivas@users.noreply.github.com>
2021-07-31 22:37:34 +0530

    Adding Array To Array List java program

---
+++ AarrayToArrayList6.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class AarrayToArrayList6 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		String [] myanimals= {"dog","cat","goat","cow","hourse","ox"};
+		
+	
+		List<String>myanimallist=Arrays.asList(myanimals);
+	
+		
+	System.out.println(myanimallist);
+	}
+
+}
