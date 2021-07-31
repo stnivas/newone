e27f316e793ab9ecfcf35b47263474a51505acaa
stnivas <86654987+stnivas@users.noreply.github.com>
2021-07-31 22:41:24 +0530

    Create javatest.java

---
+++ javatest.java
@@ -0,0 +1,23 @@
+import once.*;
+import java.util.Scanner;
+
+class javatest{
+
+public static void main(String args[]){
+
+Scanner sc=new Scanner(System.in);
+
+switch(Scanner.next()){
+{
+  case one:
+   System.out.println("exec");
+   break;
+
+case second:
+
+  System.out.println("2exe");
+  break;
+      }
+   }
+}
+
